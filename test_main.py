581ffa099b88ea6c973b666b03b1a2308830d9dc
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-09-17 12:01:07 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,5 +1,17 @@
-
-
+import main  
 if __name__ == "__main__":
+    file_path = 'playlist.csv'  # Replace with the actual path to your dataset
 
-    print("Success")
+    # Test calculate_statistics function
+    statistics_result = main.calculate_statistics(file_path)
+    print("Descriptive Statistics:")
+    print(statistics_result)
+
+    # Test visualize_data function
+    data = main.readfile(file_path)
+    main.visualize_data(data)
+
+    # Test calculate_correlation function
+    correlation_result = main.calculate_correlation(file_path)
+    print("\nCorrelation of artist_popularity with other columns:")
+    print(correlation_result)
