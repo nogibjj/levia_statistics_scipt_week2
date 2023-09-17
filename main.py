ce73adaaed89aca0b242e784b5b8315c4e684837
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-09-17 11:32:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,84 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Load the dataset (assuming trade_data.csv is your dataset file)
-df = pd.read_csv('trade_data.csv')
+# Function to read the dataset
+def readfile(a):
+    df = pd.read_csv(a)
+    return df
 
-# Calculate mean for specific columns
-mean_export = df['Export (US$ Thousand)'].mean()
-mean_import = df['Import (US$ Thousand)'].mean()
+# Function to calculate statistics for specific columns
+def calculate_statistics(file_path):
+    try:
+        # Reading the dataset from the CSV file
+        data = pd.read_csv(file_path)
 
-# Calculate median for specific columns
-median_export = df['Export (US$ Thousand)'].median()
-median_import = df['Import (US$ Thousand)'].median()
+        # Selecting specific columns of interest
+        selected_columns = ['danceability', 'energy', 'artist_popularity', 'loudness']
+        data = data[selected_columns]
 
-# Calculate standard deviation for specific columns
-std_export = df['Export (US$ Thousand)'].std()
-std_import = df['Import (US$ Thousand)'].std()
+        # Calculating mean, median, and standard deviation
+        mean = data.mean()
+        median = data.median()
+        std = data.std()
+        
+        return {'mean': mean, 'median': median, 'std': std}
+    except Exception as e:
+        return str(e)
 
-# Display the results
-print("Statistics for Export (US$ Thousand):")
-print(f"Mean: {mean_export}")
-print(f"Median: {median_export}")
-print(f"Standard Deviation: {std_export}")
+# Function to visualize specific columns as histograms
+def visualize_data(data):
+    try:
+        # Check if the input is a DataFrame
+        if not isinstance(data, pd.DataFrame):
+            raise ValueError("Input is not a pandas DataFrame")
 
-print("\nStatistics for Import (US$ Thousand):")
-print(f"Mean: {mean_import}")
-print(f"Median: {median_import}")
-print(f"Standard Deviation: {std_import}")
+        # Iterate over each numeric column and create a histogram
+        for col in data.columns:
+            plt.figure(figsize=(8, 6))
+            plt.hist(data[col], bins=20, edgecolor='k', alpha=0.7)
+            plt.xlabel(col)
+            plt.ylabel("Frequency")
+            plt.title(f"Histogram of {col}")
+            plt.grid(True)
+            plt.show()
+    
+    except Exception as e:
+        return str(e)
 
-# Create a histogram for export values
-plt.hist(df['Export (US$ Thousand)'], bins=20, edgecolor='k')
-plt.title('Export Values Distribution')
-plt.xlabel('Export (US$ Thousand)')
-plt.ylabel('Frequency')
-plt.show()
+# Function to calculate the correlation of artist_popularity with other columns
+def calculate_correlation(file_path):
+    try:
+        # Reading the dataset from the CSV file
+        data = pd.read_csv(file_path)
 
-# Create a histogram for import values
-plt.hist(df['Import (US$ Thousand)'], bins=20, edgecolor='k')
-plt.title('Import Values Distribution')
-plt.xlabel('Import (US$ Thousand)')
-plt.ylabel('Frequency')
-plt.show()
+        # Selecting specific columns of interest
+        selected_columns = ['danceability', 'energy', 'artist_popularity', 'loudness']
+        data = data[selected_columns]
 
-# Calculate the correlation between "Export (US$ Thousand)" and "Import (US$ Thousand)" columns
-correlation = df['Export (US$ Thousand)'].corr(df['Import (US$ Thousand)'])
+        # Calculating the correlation matrix
+        correlation_matrix = data.corr()
 
-# Display the correlation coefficient
-print(f"Correlation between Export and Import: {correlation}")
+        # Extracting the correlation of 'artist_popularity' with other columns
+        artist_popularity_correlation = correlation_matrix['artist_popularity']
+        
+        return artist_popularity_correlation
+    except Exception as e:
+        return str(e)
 
-# Create a scatter plot to visualize the relationship
-plt.scatter(df['Export (US$ Thousand)'], df['Import (US$ Thousand)'])
-plt.title('Scatter Plot: Export vs. Import')
-plt.xlabel('Export (US$ Thousand)')
-plt.ylabel('Import (US$ Thousand)')
-plt.show()
+if __name__ == "__main__":
+    dataset_path = "playlist.csv"
+
+    # Calculate statistics for specific columns
+    statistics_result = calculate_statistics(dataset_path)
+    print("Descriptive Statistics:")
+    print(statistics_result)
+
+    # Visualize specific columns as histograms
+    data = readfile(dataset_path)
+    visualize_data(data)
+
+    # Calculate and print the correlation of artist_popularity with other columns
+    correlation_result = calculate_correlation(dataset_path)
+    print("\nCorrelation of artist_popularity with other columns:")
+    print(correlation_result)
